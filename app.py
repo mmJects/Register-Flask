8d7138228d8f6c635c2eea5b7dc2304b9bc430c8
Yun Yun <nyunttin871@gmail.com>
2022-12-03 22:10:39 +0630

    Connecting with database to store data

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect
+from cs50 import SQL                                # importing the cs50.SQL module to connect db with python    
 
+db = SQL("mysql://root:YES@localhost:3306/db_for_flask")    # connect with the MySQL database with python
 app = Flask(__name__)
 
 app.run(debug=True)
@@ -14,14 +16,24 @@ def index():
 @app.route("/register",methods=['GET','POST'])
 def register():
     name = request.form.get("name")
-    if not name:
-        return render_template("failure.html")
     sport = request.form.get("sport")
-    if sport not in sports:
+    if not name or sport not in sports:
         return render_template("failure.html")
-    REGISTRANTS[name] = sport
-    return render_template("success.html")
+    # to store data in database , which will be permanent until it is deleted
+    db.execute("insert into registrants_froshims(name,sport) values (?,?)",name,sport)
+    # to store data in dictionary , this way is not good as it is available only once in runtime
+    # REGISTRANTS[name] = sport 
+    return redirect("/registrants")             # redirectly the user to the registrants route             
+    # return render_template("success.html")
 
 @app.route("/registrants")
 def registrants():
-    return render_template("registrants.html",registrants=REGISTRANTS)
+    registrant = db.execute("select * from registrants_froshims")          # getting the data from the database
+    return render_template("registrants.html",registrant=registrant)  
+
+@app.route("/deregister",methods=["POST"])       # deregister 
+def deregister():
+    id = request.form.get("id") # get the id
+    if id:
+        db.execute("delete from registrants_froshims where id = ?",id)  # delete the row from database
+    return redirect("/registrants")
